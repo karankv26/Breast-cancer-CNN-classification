3a95aad66987d474f89f788f0ddc466ba7853729
Karan K V <62898070+karankv26@users.noreply.github.com>
2021-08-29 13:19:02 +0530

    Create 1470.java

---
+++ 1470.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] arr = new int[nums.length];
+        int even = 0;
+        int odd = n;
+        for(int i = 0; i < nums.length ; i++){
+            if (i%2==0){
+                arr[i] = nums[even++];
+            }else{
+                arr[i] = nums[odd++];
+            }
+            }
+        return arr;
+    }
+}
